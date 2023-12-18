e4ae86d4a981c973a56c94f706da921835bb742c
Hans de Goede <hdegoede@redhat.com>
2023-12-18 19:21:07 +0100

    libcamera: debayer_cpu: Add BGR888 output support
    BGR888 is RGB888 with the red and blue pixels swapped, adjust
    the debayering to swap the red and blue pixels in the bayer pattern
    to add support for writing formats::BGR888.
    
    Signed-off-by: Hans de Goede <hdegoede@redhat.com>
    Tested-by: Bryan O'Donoghue <bryan.odonoghue@linaro.org> # sc8280xp Lenovo x13s
    Tested-by: Pavel Machek <pavel@ucw.cz>

--- include/libcamera/internal/software_isp/debayer_cpu.h
+++ include/libcamera/internal/software_isp/debayer_cpu.h
@@ -129,6 +129,7 @@ private:
 	unsigned int lineBufferIndex_;
 	unsigned int x_shift_; /* Offset of 0/1 applied to window_.x */
 	bool enableInputMemcpy_;
+	bool swapRedBlueGains_;
 	float gamma_correction_;
 	int measuredFrames_;
 	int64_t frameProcessTime_;

b5fa7fd0c1d26d375bee2267fd39817edb895e74
Hans de Goede <hdegoede@redhat.com>
2023-12-19 15:45:51 +0100

    ov01a1s HACK
    Signed-off-by: Hans de Goede <hdegoede@redhat.com>

--- src/libcamera/software_isp/debayer_cpu.cpp
+++ src/libcamera/software_isp/debayer_cpu.cpp
@@ -22,6 +22,8 @@
 
 namespace libcamera {
 
+extern bool is_ov01a1s;
+
 /**
  * \class DebayerCpu
  * \brief Class for debayering on the CPU
@@ -517,6 +519,9 @@ int DebayerCpu::getInputConfig(PixelFormat inputFormat, DebayerInputConfig &conf
 	BayerFormat bayerFormat =
 		BayerFormat::fromPixelFormat(inputFormat);
 
+	if (is_ov01a1s)
+		bayerFormat.order = BayerFormat::IGIG_GBGR_IGIG_GRGB;
+
 	if ((bayerFormat.bitDepth == 8 || bayerFormat.bitDepth == 10 || bayerFormat.bitDepth == 12) &&
 	    bayerFormat.packing == BayerFormat::Packing::None &&
 	    isStandardBayerOrder(bayerFormat.order)) {
@@ -610,6 +615,9 @@ int DebayerCpu::setDebayerFunctions(PixelFormat inputFormat, PixelFormat outputF
 	BayerFormat bayerFormat =
 		BayerFormat::fromPixelFormat(inputFormat);
 	bool addAlphaByte = false;
+
+	if (is_ov01a1s)
+		bayerFormat.order = BayerFormat::IGIG_GBGR_IGIG_GRGB;
 
 	xShift_ = 0;
 	swapRedBlueGains_ = false;

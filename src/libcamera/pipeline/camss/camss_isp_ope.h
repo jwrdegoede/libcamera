2ef342cc347293ca4194c43351c9238f8951e6c8
Hans de Goede <johannes.goede@oss.qualcomm.com>
2026-03-31 15:21:40 +0200

    camss: Add Offline Processing Engine ISP support
    This adds support for the OPE through the RFC OPE M2M kernel driver
    posted upstream recently.
    
    The M2M OPE kernel driver is mainly a RFC driver and this driver will
    need to be changed into a proper ISP driver with e.g. a second /dev/video#
    input node for passing parameter-buffers to the ISP.
    
    Likewise this libcamera OPE support should mostly be seen as a proof
    of concept of the CamssIsp virtual base class abstraction and this will
    need to change / evolve to also grow an IPA and parameter buffer support
    as the kernel driver evolves.
    
    Link: https://lore.kernel.org/linux-media/20260323125824.211615-1-loic.poulain@oss.qualcomm.com/
    Signed-off-by: Hans de Goede <johannes.goede@oss.qualcomm.com>

---
+++ src/libcamera/pipeline/camss/camss_isp_ope.h
@@ -0,0 +1,59 @@
+/* SPDX-License-Identifier: LGPL-2.1-or-later */
+/*
+ * Qualcomm CAMSS OPE ISP class
+ *
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+ */
+
+#pragma once
+
+#include <memory>
+#include <vector>
+
+#include "camss_isp.h"
+
+namespace libcamera {
+
+class CameraSensor;
+class ControlInfoMap;
+class Converter;
+class DeviceEnumerator;
+class MediaDevice;
+class PipelineHandler;
+class SoftwareIsp;
+
+class CamssIspOpe : public CamssIsp
+{
+public:
+	static std::unique_ptr<CamssIspOpe> match(PipelineHandler *pipe,
+						  DeviceEnumerator *enumerator,
+						  const CameraSensor *sensor,
+						  ControlInfoMap *ispControls);
+
+	CamssIspOpe(PipelineHandler *pipe, const CameraSensor *sensor, ControlInfoMap *ispControls);
+	~CamssIspOpe() override;
+
+	bool isValid() override;
+
+	StreamConfiguration generateConfiguration(const StreamConfiguration &raw) const override;
+	StreamConfiguration validate(const StreamConfiguration &raw, const StreamConfiguration &req) const override;
+	int configure(const StreamConfiguration &inputCfg,
+		      const StreamConfiguration &outputCfg) override;
+
+	int exportOutputBuffers(const Stream *stream, unsigned int count,
+				std::vector<std::unique_ptr<FrameBuffer>> *buffers) override;
+	void queueBuffers(Request *request, FrameBuffer *input) override;
+
+	void processStats(const uint32_t frame, const uint32_t bufferId,
+			  const ControlList &sensorControls) override;
+
+	int start() override;
+	void stop() override;
+
+private:
+	std::shared_ptr<MediaDevice> opeMediaDev_;
+	std::unique_ptr<Converter> converter_;
+	const CameraSensor *sensor_;
+};
+
+} /* namespace libcamera */
